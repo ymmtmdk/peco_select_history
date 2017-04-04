a59ac44d1190604c501a230e280af209adb3ac4f
ymmtmdk <ymmtmdk@gmail.com>
2017-04-04 21:19:03 +0900

    Add fish_history.c

--- peco_select_history.fish
+++ peco_select_history.fish
@@ -1,17 +1,26 @@
 function peco_select_history
-  if test (count $argv) = 0
-    set peco_flags --layout=bottom-up
-  else
-    set peco_flags --layout=bottom-up --query "$argv"
+  if test (count $argv) -gt 0
+    set flags --query "$argv"
   end
 
   history merge
-  history --show-time='%y-%m-%d %H:%M> ' | peco $peco_flags | sed -e "s/.\{16\}//"| read foo
+  history --show-time='%y-%m-%d %H:%M> '| peco --layout=bottom-up $flags | sed -e "s/.\{16\}//"| read foo
 
   if [ $foo ]
     commandline $foo
-  else
-    commandline ''
+  end
+end
+
+function peco_select_history_with_c
+  if test (count $argv) -gt 0
+    set flags --query "$argv"
+  end
+
+  history save
+  fish_history ~/.local/share/fish/fish_history | peco --layout=bottom-up $flags | sed -e "s/.\{16\}//"| read foo
+
+  if [ $foo ]
+    commandline $foo
   end
 end
 
