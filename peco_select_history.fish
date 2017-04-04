abb4d2e278469ddd20ebcf3660981c6dda79946c
ymmtmdk <ymmtmdk@gmail.com>
2017-04-04 21:26:58 +0900

    Update peco_select_history.fish

--- peco_select_history.fish
+++ peco_select_history.fish
@@ -10,17 +10,3 @@ function peco_select_history
     commandline $foo
   end
 end
-
-function peco_select_history_with_c
-  if test (count $argv) -gt 0
-    set flags --query "$argv"
-  end
-
-  history save
-  fish_history ~/.local/share/fish/fish_history | peco --layout=bottom-up $flags | sed -e "s/.\{16\}//"| read foo
-
-  if [ $foo ]
-    commandline $foo
-  end
-end
-
